a44398d1ea3b0865657639230d3a9974cf327d90
Martin Aryee <martin.aryee@gmail.com>
2017-10-17 23:55:22 -0400

    Added hicpro annotation (resfrag bed and chrom sizes) for hg38 and mm10

---
+++ Docker/hicpro/Mboi_mm10_gen.R
@@ -0,0 +1,15 @@
+require(HiTC)
+require(rtracklayer)
+require(BSgenome.Mmusculus.UCSC.mm10)
+
+## Generate restriction fragment file
+human_chr <- seqlevels(BSgenome.Mmusculus.UCSC.mm10)[1:21]
+resFrag <- getRestrictionFragmentsPerChromosome(resSite="GATC", chromosomes=human_chr, overhangs5=1, genomePack="BSgenome.Mmusculus.UCSC.mm10")
+allRF <- do.call("c",resFrag)
+names(allRF) <- unlist(sapply(resFrag, function(x){paste0("HIC_", seqlevels(x), "_", 1:length(x))}))
+export(allRF, format="bed", con="Mboi_resfrag_mm10.bed")
+
+## Generate chromosome size file
+chr <- seqlevels(BSgenome.Mmusculus.UCSC.mm10)[1:21]
+chrom.size <- seqlengths(BSgenome.Mmusculus.UCSC.mm10)[chr]
+write.table(chrom_size, file="chrom_mm10.sizes", quote=FALSE, col.names=FALSE, sep="\t")
