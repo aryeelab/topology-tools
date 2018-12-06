9ef0a34cb34cd686ccc2bdeb6d0dc1ed508f5940
Martin Aryee <martin.aryee@gmail.com>
2018-12-06 00:38:15 -0500

    Fixed MboI restriction fragment bug (fragment edges were off by 1 base)

--- Docker/hicpro/Mboi_grch38_gen.R
+++ Docker/hicpro/Mboi_grch38_gen.R
@@ -6,7 +6,7 @@ require(BSgenome.Hsapiens.NCBI.GRCh38)
 
 ## Generate restriction fragment file
 human_chr <- seqlevels(Hsapiens)[1:24]
-resFrag <- getRestrictionFragmentsPerChromosome(resSite="GATC", chromosomes=human_chr, overhangs5=1, genomePack="BSgenome.Hsapiens.NCBI.GRCh38")
+resFrag <- getRestrictionFragmentsPerChromosome(resSite="GATC", chromosomes=human_chr, overhangs5=0, genomePack="BSgenome.Hsapiens.NCBI.GRCh38")
 allRF <- do.call("c",resFrag)
 names(allRF) <- unlist(sapply(resFrag, function(x){paste0("HIC_", seqlevels(x), "_", 1:length(x))}))
 export(allRF, format="bed", con="Mboi_resfrag_grch38.bed")
