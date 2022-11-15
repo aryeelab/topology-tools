366842ac61ebb8f83da6e3eb45087f4d3a6891a5
Martin Aryee <martin.aryee@gmail.com>
2022-11-15 13:01:57 -0500

    Moved higlass docker to a separate repo

--- Docker/higlass/ingest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for MCOOL in /mcool/*.mcool; do
-    BASENAME=$(basename $MCOOL)
-    SAMPLE_ID=${BASENAME%.*} # Remove extension
-    echo $SAMPLE_ID
-    python higlass-server/manage.py ingest_tileset --filename $MCOOL --name $SAMPLE_ID --datatype matrix --filetype cooler 
-done
