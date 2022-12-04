1049721cd4d6990c7788a4fbfb51de6f5b1e62bc
Martin Aryee <martin.aryee@gmail.com>
2022-12-04 14:16:18 -0500

    Fixed monitor_v3 disk_free grep

--- tests/monitor_v3.sh
+++ tests/monitor_v3.sh
@@ -9,15 +9,14 @@ echo "#Total Disk space: $(df -h | grep cromwell_root | awk '{ print $2}')"
 echo "#"
 
 
-echo "#time cpu_usage_percent mem_free_gb disk_free_gb"
+echo "#time cpu_usage_percent mem_avail_gb disk_free_gb"
 
 function runtimeInfo() {
         datetime=$(date "+%F_%T")
-        cpu=$(top -bn 2 -d 0.01 | grep '^%Cpu' | tail -n 1 | awk '{print $2}')%
-        mem_free=$(free -g | grep Mem | awk '{print $4}')
-        #disk_free=$(df -BG | grep cromwell_root | awk '{ print $4 }')
-        disk_free=$(df -BG | grep virtiofs0 | awk '{ print $4 }' | tr -d G)
-        echo $datetime $cpu $mem_free $disk_free
+        cpu=$(top -bn 2 -d 0.01 | grep '^%Cpu' | tail -n 1 | awk '{print $2}')
+        mem_avail=$(free -g | grep Mem | awk '{print $7}')
+        disk_free=$(df -BG | grep cromwell | awk '{ print $4 }' | tr -d G)
+        echo $datetime $cpu $mem_avail $disk_free
 }
 
-while true; do runtimeInfo; sleep 5; done
+while true; do runtimeInfo; sleep 60; done
